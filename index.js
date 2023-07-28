d7fff5c34f21cc98daf1ee049c48a93207cd52bd
isaacmayela <isaacmayela27@gmail.com>
2023-07-28 16:29:40 +0200

    feat : i've tried to do that with objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("bonjour")
+function mixed_message(obj){
+
+}
+
+const sentence = {
+    elementS1 : {
+        subjet :"Marceline",
+        verb : "est",
+        complement : "gentille"
+    },
+    elementS2 : {
+        subjet :"Patrick",
+        verb :"",
+        complement : ""
+    },
+    elementS3:{
+        subjet :"",
+        verb :"",
+        complement : ""
+    },
+    elementS3:{
+        subjet :"",
+        verb :"",
+        complement : ""
+    }
+}
+
