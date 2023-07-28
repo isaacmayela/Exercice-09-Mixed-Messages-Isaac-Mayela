9fbf04ffdcb330101f2ae06b2116ff383d78b6b0
isaacmayela <isaacmayela27@gmail.com>
2023-07-28 17:11:19 +0200

    feat : trie with arrays

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-function mixed_message(obj){
+function mixed_message(){
+    list1 = ["J'aime","Tu adores","Samar s'amuse à","Joseph a fini de"]
+    list2 = ["nager", "parler à tes collègues","jouer à la console","préparer"]
+    list3 = ["avec cette culotte bleu","comme d'habitude","chez moi",", c'est bien !"]
 
+    let random_index = Math.floor(Math.random() * list1.length)
+
+    console.log(random_index);
 }
 
-const sentence = {
-    elementS1 : {
-        subjet :"Marceline",
-        verb : "est",
-        complement : "gentille"
-    },
-    elementS2 : {
-        subjet :"Patrick",
-        verb :"",
-        complement : ""
-    },
-    elementS3:{
-        subjet :"",
-        verb :"",
-        complement : ""
-    },
-    elementS3:{
-        subjet :"",
-        verb :"",
-        complement : ""
-    }
-}
+
+mixed_message()
+
+// const sentence = {
+//     elementS1 : {
+//         subjet :"Marceline",
+//         verb : "est",
+//         complement : "gentille"
+//     },
+//     elementS2 : {
+//         subjet :"Patrick",
+//         verb :"",
+//         complement : ""
+//     },
+//     elementS3:{
+//         subjet :"",
+//         verb :"",
+//         complement : ""
+//     },
+//     elementS3:{
+//         subjet :"",
+//         verb :"",
+//         complement : ""
+//     }
+// }
 
