293dfce3b7f67fecdd292d90ac08a99fd90cd6ed
isaacmayela <isaacmayela27@gmail.com>
2023-07-28 17:45:47 +0200

    feat : this is my last version

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function index_random(list){
 }
 
 function mixed_message(){
-    list1 = ["J'aime","Tu adores","Samar s'amuse à","Joseph a fini de"]
-    list2 = ["nager", "parler à tes collègues","jouer à la console","préparer"]
-    list3 = ["avec cette culotte bleu","comme d'habitude","chez moi",", c'est bien !"]
+    element1 = ["J'aime","Tu adores","Samar s'amuse à","Joseph a fini de"]
+    element2 = ["nager", "parler à tes collègues","jouer à la console","préparer"]
+    element3 = ["avec cette culotte bleu","comme d'habitude","chez moi",", c'est bien !"]
 
 
     let message = list1[index_random(list1)] + " " + list2[index_random(list2)]+ " " + list3[index_random(list3)]
@@ -15,27 +15,3 @@ function mixed_message(){
 
 
 mixed_message()
-
-// const sentence = {
-//     elementS1 : {
-//         subjet :"Marceline",
-//         verb : "est",
-//         complement : "gentille"
-//     },
-//     elementS2 : {
-//         subjet :"Patrick",
-//         verb :"",
-//         complement : ""
-//     },
-//     elementS3:{
-//         subjet :"",
-//         verb :"",
-//         complement : ""
-//     },
-//     elementS3:{
-//         subjet :"",
-//         verb :"",
-//         complement : ""
-//     }
-// }
-
